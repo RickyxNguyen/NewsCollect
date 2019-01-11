a742fd938c8b6637fa690ded2ba4aa03646a3315
Ricky Nguyen <ricky.nguyen@me.com>
2019-01-11 17:33:25 -0600

    Server

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds027165.mlab.com:27165/heroku_c020jhjn");
 var results = [];
 
 // Routes
